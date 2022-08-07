1c0b59b1794ad49271bab9a4f9176a0025bdb394
Jan Grosser <email@jan-grosser.de>
2022-08-07 14:43:42 +0200

    Added method set_output_io_pulse_width() (issue #13)

--- tests/test_PyVoiceRecognitionV3.py
+++ tests/test_PyVoiceRecognitionV3.py
@@ -3,6 +3,7 @@ import unittest
 
 sys.path.append('../.')
 from PyVoiceRecognitionV3 import PyVoiceRecognitionV3, MySerMock
+from PyVoiceRecognitionV3 import BadPulseWidth
 
 # Mockup for serial device
 mockdev = MySerMock()
@@ -17,9 +18,8 @@ class Test_init(unittest.TestCase):
 
     def test_isinstance(self):
         """
-        Test if correct instance is returned
+        __init__(): Test if correct instance is returned
         """
-        print("Test if correct instance is returned")
         self.assertIsInstance(
                 PyVoiceRecognitionV3(device=mockdev),
                 PyVoiceRecognitionV3
@@ -32,9 +32,8 @@ class Test_send_cmd(unittest.TestCase):
 
     def test_send_recv(self):
         """
-        Test basic communication with method send_cmd()
+        send_cmd(): Test basic communication
         """
-        print("Test basic communication with method send_cmd()")
         cmd = b'\xaa\x02\x00\x0a'    # check sys settings (00)
         exp_rsp = [bytearray(b'\xaa\x08\x00\x00\x00\x00\x00\x00\x00\x0a')]
         for msg in exp_rsp:
@@ -43,6 +42,40 @@ class Test_send_cmd(unittest.TestCase):
         rsp = vr.send_cmd(cmd)
 
         self.assertEqual(rsp,exp_rsp)
+
+class Test_set_output_io_pulse_width(unittest.TestCase):
+    """
+    Tests for method set_output_io_pulsewidth()
+    """
+
+    def test_no_argument(self):
+        """
+        set_output_io_pulse_width(): No argument
+        """
+        self.assertRaises(BadPulseWidth, vr.set_output_io_pulse_width, None)
+
+    def test_bad_pulsewidth(self):
+        """
+        set_output_io_pulse_width(): Bad argument/pulsewidth
+        """
+        # 42 ms in not a valid value for the pulse width
+        self.assertRaises(BadPulseWidth, vr.set_output_io_pulse_width, 42)
+
+    def test_valid_pulsewidth(self):
+        """
+        set_output_io_pulse_width(): Valid argument/pulsewidth
+        """
+        pw = 20 # milliseconds
+        pw_hex = hex(2) # corresponding hex value
+        mod_rsp = bytearray(b'\xaa\x03\x13\x00\x0a')
+        mockdev.append_to_inbuffer(mod_rsp)
+        exp_rsp = {
+            "raw": [mod_rsp],
+            "output_io_pulse_width_hex": pw_hex,
+            "output_io_pulse_width_ms": pw,
+                }
+        rsp = vr.set_output_io_pulse_width(pulsewidth = pw)
+        self.assertEqual(rsp, exp_rsp)
 
 if __name__ == '__main__':
     unittest.main()
